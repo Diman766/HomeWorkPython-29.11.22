e0c8151096e162d5cb61041e394d6acf2453cb95
unknown <Dmitiy_yar@mail.ru>
2022-11-30 13:12:14 +0300

    add fixed task3

--- task3.py
+++ task3.py
@@ -1,11 +1,12 @@
-# Задайте список из вещественных чисел. 
+# Задайте список из вещественных чисел.
 # Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
 
 lst = [1.1, 1.2, 3.1, 5, 10.01]
 for i in range(0, len(lst)-1):
     if type(lst[i]) == int:
         lst.pop(i)
-lst = [str(lst[i]) for i in range(0, len(lst))]
+
+lst = list(map(str, lst))
 
 lstResult = []
 
@@ -14,6 +15,6 @@ for i in range(0, len(lst)):
     lstResult.append((tmp[1]))
 
 lstResult = ['0.' + lstResult[i] for i in range(0, len(lstResult))]
-lstResult = [float(lstResult[i]) for i in range(0, len(lstResult))]
-   
+lstResult = list(map(float, lstResult))
+
 print(max(lstResult) - min(lstResult))
